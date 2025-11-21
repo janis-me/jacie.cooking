cee191504f7d657eaaec2396be50781efc5f1e2f
Janis Jansen <oss@janis.me>
2025-11-21 10:25:32 +0100

    chore: update astro config

--- packages/site/astro.config.mjs
+++ packages/site/astro.config.mjs
@@ -1,18 +1,17 @@
 // @ts-check
-import { dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
 import sitemap from '@astrojs/sitemap';
 import scriptEmbed from '@brandonaaron/astro-script-embed';
 import webmanifest from 'astro-webmanifest';
-import { defineConfig, passthroughImageService } from 'astro/config';
+import { defineConfig } from 'astro/config';
 import surimi from 'vite-plugin-surimi';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jacie.cooking',
+
+  build: {
+    inlineStylesheets: 'always',
+  },
 
   integrations: [
     sitemap(),
